cf0aba8102e4cd68511f94e608a930bf0e3252c4
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-01 16:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let div=document.querySelector('sizeInfo');
+let h1=div.firstChild;
+window.addEventListener('resize',()=>{
+	let width =window.innerWidth;
+let height = window.innerHeight;
+	h1.innerText="width: "+width+" and Height: "+height;
+	
+})
